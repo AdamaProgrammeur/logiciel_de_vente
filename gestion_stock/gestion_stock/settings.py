808becd20be28fe8b98543941d01d85b571ec241
AdamaProgrammeur <informaticiendoucoure@gmail.com>
2026-04-08 14:53:28 +0000

    Update settings.py

--- gestion_stock/gestion_stock/settings.py
+++ gestion_stock/gestion_stock/settings.py
@@ -12,7 +12,7 @@ load_dotenv(BASE_DIR.parent / '.env')
 # SECURITY
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ['stockpro.onrender.com']
+ALLOWED_HOSTS = ['stockpro-ep40.onrender.com']
 
 # Applications
 INSTALLED_APPS = [
